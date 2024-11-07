3b380bb6f783dc552a9cf1c01185418ddc2c14c4
FSD7-Boy <boyzasad01@gmail.com>
2024-11-07 14:09:09 +0700

    fix: import express

--- app.mjs
+++ app.mjs
@@ -1,12 +1,10 @@
 import express from "express";
 import cors from "cors";
-
 const app = express();
 const port = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
-
 app.get("/profiles", (req, res) => {
   return res.json({
     data: {
