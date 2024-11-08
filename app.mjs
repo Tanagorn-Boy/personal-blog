80b84e687ebaf2f1e5e0a3d2491edc32190d0340
FSD7-Boy <boyzasad01@gmail.com>
2024-11-08 14:51:42 +0700

    feat: add function to insert new post data into the database

--- app.mjs
+++ app.mjs
@@ -1,16 +1,37 @@
 import express from "express";
 import cors from "cors";
+import connectionPool from "./utils/db.mjs";
+
 const app = express();
 const port = process.env.PORT || 4001;
 
 app.use(cors());
 app.use(express.json());
-app.get("/profiles", (req, res) => {
-  return res.json({
-    data: {
-      name: "john",
-      age: 20,
-    },
+
+app.post("/posts", async (req, res) => {
+  const newPost = req.body;
+
+  try {
+    const query = `insert into posts (title, image, category_id, description, content, status_id) values ($1, $2, $3, $4, $5, $6)`;
+
+    const values = [
+      newPost.title,
+      newPost.image,
+      newPost.category_id,
+      newPost.description,
+      newPost.content,
+      newPost.status_id,
+    ];
+
+    await connectionPool.query(query, values);
+  } catch (e) {
+    return res.status(500).json({
+      message: `Server could not create post because database connection ${e}`,
+    });
+  }
+
+  return res.status(201).json({
+    message: "Created post sucessfully",
   });
 });
 
